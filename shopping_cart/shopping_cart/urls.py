0713a69ea9b6811e4c91459ee55ed657b853139f
Lu Daven <daven.l@triplet-rd.com>
2021-11-17 17:01:39 +0900

    added comments for easier navigation

--- shopping_cart/shopping_cart/urls.py
+++ shopping_cart/shopping_cart/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# /admin brings us to the main admin panel
+# /shop brings us to the main api
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('shop/', include('api.urls')),
 ]
